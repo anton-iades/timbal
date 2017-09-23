0f7defebc1b29daa629513e15ba49ad950f9b6f5
Anton Antoniades <a.antoniades.85@gmail.com>
2017-09-23 13:22:14 +1000

    Created solution and allocator class with red test

---
+++ Timbal.Tests/AllocatorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Timbal.Utilities;
+
+namespace Timbal.Tests
+{
+    [TestClass]
+    public class AllocatorTests
+    {
+        [TestMethod]
+        public void should_allocate_evenly()
+        {
+            // arrange
+            var src = new[] { 111, 211, 322, };
+            const decimal amountToAllocate = 10m;
+            const int allocationPrecision = 4;
+
+            // act
+            var actual = src.AllocateAmountEvenly(amountToAllocate, allocationPrecision);
+
+            // assert
+            Assert.AreEqual<int>(3, actual.Count);
+            Assert.AreEqual<decimal>(amountToAllocate, actual.Values.Sum());
+
+            Assert.IsTrue(actual.ContainsKey(1));
+            Assert.AreEqual<decimal>(3.3334m, actual[1]);
+
+            Assert.IsTrue(actual.ContainsKey(2));
+            Assert.AreEqual<decimal>(3.3333m, actual[2]);
+
+            Assert.IsTrue(actual.ContainsKey(3));
+            Assert.AreEqual<decimal>(3.3333m, actual[3]);
+        }
+    }
+}
---
+++ Timbal.Utilities/Allocator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timbal.Utilities
+{
+    public static class Allocator
+    {
+        public static IDictionary<int, decimal> AllocateAmountEvenly(this int[] recipients, decimal amount, int precision)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
