13b29129c835795d2e6647d2f62ac76643c0c636
Anton Antoniades <a.antoniades.85@gmail.com>
2017-09-24 23:20:15 +1000

    Refactored allocate evenly to work via allocate proportionally

--- Timbal.Utilities/Allocator.cs
+++ Timbal.Utilities/Allocator.cs
@@ -8,20 +8,9 @@ namespace Timbal.Utilities
     {
         public static IEnumerable<KeyValuePair<int, decimal>> AllocateAmountEvenly(this IEnumerable<int> recipients, decimal amount, int precision)
         {
-            if (recipients == null) throw new ArgumentNullException(nameof(recipients));
-            var recipientsArray = recipients.ToArray();
-
-            if (recipientsArray.Length == 0) yield break;
-
-            var generalAllocation = decimal.Round(amount / recipientsArray.Length, precision);
-            var remainder = amount % generalAllocation;
-
-            for (int i = 0; i < recipientsArray.Length; i++)
-            {
-                var allocation = i == 0 ? generalAllocation + remainder : generalAllocation;
-
-                yield return new KeyValuePair<int, decimal>(recipientsArray[i], allocation);
-            }
+            return recipients
+                .Select(r => new KeyValuePair<int, decimal>(r, 1m))
+                .AllocateProportionally(amount, precision);
         }
 
         public static IEnumerable<KeyValuePair<int, decimal>> AllocateProportionally(this IEnumerable<KeyValuePair<int, decimal>> recipientAllocationBasis, decimal amount, int precision)
