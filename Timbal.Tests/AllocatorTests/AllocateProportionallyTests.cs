1abaf9bf50f5500e0407ce0fc758265f4aed1072
Anton Antoniades <a.antoniades.85@gmail.com>
2017-09-24 23:19:44 +1000

    Tests + implementation for allocate proportionally

---
+++ Timbal.Tests/AllocatorTests/AllocateProportionallyTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Timbal.Utilities;
+
+namespace Timbal.Tests.AllocatorTests
+{
+    [TestClass]
+    public class AllocateProportionallyTests
+    {
+        [TestMethod]
+        public void should_allocate_propotionally()
+        {
+            // arrange
+            const int precision = 5;
+            const decimal amountToAllocate = 250m;
+            var allocationBasis = new Dictionary<int, decimal>
+            {
+                [101] = 2,
+                [202] = 5
+            };
+
+            // act
+            var actual = allocationBasis.AllocateProportionally(amountToAllocate, precision).ToList();
+
+            // assert
+            Assert.AreEqual<int>(2, actual.Count());
+            Assert.AreEqual<decimal>(amountToAllocate, actual.Sum(k => k.Value));
+
+            Assert.IsTrue(actual.Any(k => k.Key == 101 && k.Value == 71.42857m));
+            Assert.IsTrue(actual.Any(k => k.Key == 202 && k.Value == 178.57143m));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void should_throw_arg_null()
+        {
+            // arrange
+            IEnumerable<KeyValuePair<int, decimal>> src = null;
+            const decimal amountToAllocate = 10m;
+            const int allocationPrecision = 4;
+
+            // act
+            var actual = src.AllocateProportionally(amountToAllocate, allocationPrecision).ToList();
+
+            // assert
+        }
+
+        [TestMethod]
+        public void should_return_empty_collection()
+        {
+            // arrange
+            var src = Enumerable.Empty<KeyValuePair<int, decimal>>();
+            const decimal amountToAllocate = 10m;
+            const int allocationPrecision = 4;
+
+            // act
+            var actual = src.AllocateProportionally(amountToAllocate, allocationPrecision).ToList();
+
+            // assert
+            Assert.AreEqual<int>(0, actual.Count());
+        }
+    }
+}
--- Timbal.Utilities/Allocator.cs
+++ Timbal.Utilities/Allocator.cs
@@ -23,5 +23,27 @@ namespace Timbal.Utilities
                 yield return new KeyValuePair<int, decimal>(recipientsArray[i], allocation);
             }
         }
+
+        public static IEnumerable<KeyValuePair<int, decimal>> AllocateProportionally(this IEnumerable<KeyValuePair<int, decimal>> recipientAllocationBasis, decimal amount, int precision)
+        {
+            if (recipientAllocationBasis == null) throw new ArgumentNullException(nameof(recipientAllocationBasis));
+            var allocationBasisArray = recipientAllocationBasis.ToArray();
+
+            if (allocationBasisArray.Length == 0) yield break;
+
+            // TODO: handle total is zero
+            // TODO: handle negative allocation basis
+            var total = allocationBasisArray.Sum(k => k.Value);
+
+            var rv = allocationBasisArray.Select(k => new KeyValuePair<int, decimal>(k.Key, decimal.Round(k.Value / total * amount, precision))).ToArray();
+            var remainder = amount - rv.Sum(k => k.Value);
+
+            for (int i = 0; i < rv.Length; i++)
+            {
+                var key = rv[i].Key;
+                var val = i == 0 ? rv[i].Value + remainder : rv[i].Value;
+                yield return new KeyValuePair<int, decimal>(key, val);
+            }
+        }
     }
 }
