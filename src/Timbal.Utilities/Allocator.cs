546f7d58667c1a5b280b245cdad38d357f49fdb7
anton.antoniades@zoho.com <anton.antoniades@zoho.com>
2020-08-23 18:05:48 +1000

    Make allocator take generic key

--- src/Timbal.Utilities/Allocator.cs
+++ src/Timbal.Utilities/Allocator.cs
@@ -6,33 +6,47 @@ namespace Timbal.Utilities
 {
     public static class Allocator
     {
-        public static IEnumerable<KeyValuePair<int, decimal>> AllocateAmountEvenly(this IEnumerable<int> recipients, decimal amount, int precision)
+        public static IEnumerable<KeyValuePair<TKey, decimal>> AllocateAmountEvenly<TKey>(this IEnumerable<TKey> recipients, decimal amountToAllocate, int precision)
         {
             return recipients
-                .Select(r => new KeyValuePair<int, decimal>(r, 1m))
-                .AllocateProportionally(amount, precision);
+                .Select(r => new KeyValuePair<TKey, decimal>(r, 1m))
+                .AllocateProportionally(amountToAllocate, precision);
         }
 
-        public static IEnumerable<KeyValuePair<int, decimal>> AllocateProportionally(this IEnumerable<KeyValuePair<int, decimal>> recipientAllocationBasis, decimal amount, int precision)
+        public static IEnumerable<KeyValuePair<TKey, decimal>> AllocateProportionally<TKey>(this IEnumerable<KeyValuePair<TKey, decimal>> recipientWeights, decimal amountToAllocate, int precision)
         {
-            if (recipientAllocationBasis == null) throw new ArgumentNullException(nameof(recipientAllocationBasis));
-            var allocationBasisArray = recipientAllocationBasis.ToArray();
-
-            if (allocationBasisArray.Length == 0) throw new ArgumentException(Errors.NO_RECIPIENTS);
-
-            var total = allocationBasisArray.Sum(k => k.Value);
-
-            if (total == 0m) throw new ArgumentException(Errors.ALLOCATION_BASIS_ZERO);
-
-            var rv = allocationBasisArray.Select(k => new KeyValuePair<int, decimal>(k.Key, decimal.Round(k.Value / total * amount, precision))).ToArray();
-            var remainder = amount - rv.Sum(k => k.Value);
-
-            for (int i = 0; i < rv.Length; i++)
+            if (recipientWeights is null)
             {
-                var key = rv[i].Key;
-                var val = i == 0 ? rv[i].Value + remainder : rv[i].Value;
-                yield return new KeyValuePair<int, decimal>(key, val);
+                throw new ArgumentNullException(nameof(recipientWeights));
             }
+
+            var rw = recipientWeights.ToList();
+
+            if (!rw.Any())
+            {
+                throw new ArgumentException(Errors.NO_RECIPIENTS);
+            }
+
+            var totalWeight = rw.Sum(k => k.Value);
+
+            if (totalWeight == decimal.Zero)
+            {
+                throw new ArgumentException(Errors.ALLOCATION_BASIS_ZERO);
+            }
+
+            var weightMultiplier = amountToAllocate / totalWeight;
+
+            var rv = rw
+                .Select(weight => new KeyValuePair<TKey, decimal>(weight.Key, decimal.Round(weight.Value * weightMultiplier, precision)))
+                .ToList();
+
+            var remainder = amountToAllocate - rv.Sum(k => k.Value);
+            if (remainder != decimal.Zero)
+            {
+                rv[0] = new KeyValuePair<TKey, decimal>(rv[0].Key, rv[0].Value + remainder);
+            }
+
+            return rv;
         }
     }
 }
